188632e69ea1cd9791e78b1cae11826f0414eb65
aakashharpathkar-portfolio <60968409+aakashharpathkar-portfolio@users.noreply.github.com>
2020-02-12 17:47:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+postToGoogle();
+
+function postToGoogle() {
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+    $.ajax({
+        url: "https://docs.google.com/forms/u/4/d/e/1FAIpQLSeJk60lLtw7lhawNqx4tYG3D8Mi5CONwi6sKTvZDZL9t0caDA/formResponse?",
+        data: { "entry.1755576767": name, "entry.914317674": email, "entry.1284007056": message},
+        type: "POST",
+        dataType: "xml",
+        success: function (res) {
+            console.log(res);
+        }
+    });
+    return false;
+}
